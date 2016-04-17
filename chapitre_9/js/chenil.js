7b68308d670543e0665640bb3a41bc63214d56d9
jerome-bouvier <moeb.bzhash@gmail.com>
2016-04-17 17:47:58 +0200

    chapitre 9 fin

---
+++ chapitre_9/js/chenil.js
@@ -0,0 +1,33 @@
+var Chien = {
+    // initialise le chien
+    init: function (nom, race, taille) {
+        this.nom = nom;
+        this.race = race;
+        this.taille = taille;
+    },
+    // Renvoie l'aboiement du chien
+    aboyer: function () {
+        var aboiement = "Whoua ! Whoua !";
+        if (this.taille < 25) {
+            aboiement = "Kaii ! Kaii !";
+        } else if (this.taille > 60) {
+            aboiement = "Grrr ! Grrr !";
+        }
+        return aboiement;
+    }
+};
+
+var chien1 = Object.create(Chien);
+chien1.init("Crokdur", "mâtin de Naples", 75);
+var chien2 = Object.create(Chien);
+chien2.init("Pupuce", "bichon", 22);
+var chien3 = Object.create(Chien);
+chien3.init("Médor", "labrador", 58);
+
+var chenil = [chien1, chien2, chien3];
+
+console.log("Le chenil héberge " + chenil.length + " chien(s) :");
+
+chenil.forEach(function (chien) {
+    console.log(chien.nom + " est un " + chien.race + " mesurant " + chien.taille + " cm. Il aboie : " + chien.aboyer());
+});
--- chapitre_9/js/films.js
+++ chapitre_9/js/films.js
@@ -1,11 +1,23 @@
-var Film {
+var Film = {
     init: function (titre, annee, real) {
         this.titre = titre;
         this.annee = annee;
         this.real = real;
-    },
+    }
 };
 
 
-var films [];
+var film1 = Object.create(Film);
+film1.init("alien 1", 2000, "Ridley Scott");
 
+var film2 = Object.create(Film);
+film2.init("Wall Street", 2013, "Martin Scorsese");
+
+var film3 = Object.create(Film);
+film3.init("Vice-Versa", 2015, "Pete Docter");
+
+var films = [film1, film2, film3];
+
+films.forEach(function (film) {
+    console.log(film.titre + "(" + film.annee + ", " + film.real + ")")
+});
